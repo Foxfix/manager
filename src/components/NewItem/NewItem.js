b1b0d8d79b99b5a0fb21f54aebcce1dc12c488de
olga <jazzzz111@gmail.com>
2018-11-12 09:57:45 +0200

    added form to add new tasks

--- src/components/NewItem/NewItem.js
+++ src/components/NewItem/NewItem.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './NewItem.css'
 
-const NewItem = (props) => (
-  <div>
-    <input />
-    <button
-      type="button"
-      className=" NewItem btn btn-info btn-sm float-right"
-      onClick={() => props.onAdded("kjhgkjhgk")}
-    >
-      Add new task
-      <i className="fa fa-plus" aria-hidden="true"></i>
-    </button>
-  </div>
+export default class NewItem extends Component {
+  state = {
+    label: ''
+  };
 
-);
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  };
 
-export default NewItem;
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdded(this.state.label)
+  };
+
+  render() {
+    return (
+      <form className="NewItem d-flex"
+      onSubmit={this.onSubmit}>
+        <input type="text" className="form-control"
+          onChange={this.onLabelChange}
+               placeholder="What I have to do ..."/>
+        <button
+          className="btn btn-info  float-right">
+          Add new task
+          <i className="fa fa-plus" aria-hidden="true"></i>
+        </button>
+
+      </form>
+    )
+  }
+};
